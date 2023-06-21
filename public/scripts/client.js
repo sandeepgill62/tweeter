62fd9d1470647e8cfdc547c6164fbd9e1695a145
Sandeep Gill <sandeepgill62@gmail.com>
2023-06-21 05:06:57 +0000

    Dynamic Tweets: adding code in client.js using ajax, reading JSON data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,69 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $t = createTweetElement(tweet);
+    $(document).ready(function() {
+      $('.tweets-container').append($t);
+    });
+  }
+}
+
+const createTweetElement = function(tweet) {
+
+  let $tweet = $(`<div class="tweets-outline">
+  <article class="tweets">
+    <!-- header of tweet -->
+    <header>
+      <div class="avtarAndName">
+        <img src=${tweet['user']['avatars']}>
+        <label for="tweeter-name">${tweet['user']['name']}</label>
+      </div>
+      <label class="tweeter-tag" for="tweeter-tag">${tweet['user']['handle']}</label>
+    </header>
+
+    <!-- message body of tweet -->
+    <div class="tweet-message">
+      <label for="tweeter-days">${tweet['content']['text']}</label>
+    </div>
+
+    <!-- footer of tweet -->
+    <footer>
+      <label for="tweet-days">${tweet['created_at']}</label>
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-sharp fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+</div>`);
+  return $tweet;
+}
+
+renderTweets(data);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,22 @@
 
 $(document).ready(function() {
   // get length of maximum character
-  var maxLength = $('output').val();
+  var maxLength = $('.counter').val();
 
   // trigger the event handler when any key press
   $("textarea").keypress(function() {
     //get remaining value of character of text area
     var textlen = maxLength - $(this).val().length;
     //set the value
-    $('output').text(textlen);
+    $('.counter').text(textlen);
 
     //set the color of output variable accoding to value
-    if ($('output').val() < 0) {
+    if ($('.counter').val() < 0) {
       // set up red
-      $('output').css('color', '#FF0000');
+      $('counter').css('color', '#FF0000');
     } else {
       //set up black
-      $('output').css('color', '#000000');
+      $('counter').css('color', '#000000');
     }
   });
 });
